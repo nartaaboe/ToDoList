67ff0f25e967dbe0c9e7f7ca914d35b09e14b928
Nartai <kanosadventure@gmail.com>
2024-07-24 21:35:23 +0600

    finished

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,11 +1,16 @@
-function ToDoItem({task}){
+function ToDoItem({task, deleteTask, toggleCompleted}){
+    function handleChange(){
+        toggleCompleted(task.id);
+    }
     return (
         <div className="todo-item">
             <input
             type="checkbox"
             checked={task.completed}
+            onChange={handleChange}
             />
             <p>{task.text}</p>
+            <button onClick={() => deleteTask(task.id)}>X</button>
         </div>
     )
 }
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -19,17 +19,29 @@ function ToDoList(){
         setTasks([...tasks, newTask]);
         setText('');
     }
+    function deleteTask(id){
+        setTasks(tasks.filter(task => task.id !== id));
+    }
+    function toggleCompleted(id){
+        setTasks(tasks.map(task => {
+            if(task.id === id)
+                return {...task, completed: !task.completed};
+            return task;
+        }));
+    }
     return (
         <div className="todo-list">
             {tasks.map(task => (
                 <ToDoItem
                     key = {task.id}
                     task = {task}
+                    deleteTask = {deleteTask}
+                    toggleCompleted = {toggleCompleted}
                 />
             ))}
-            <input 
-            value={text}
-            onChange={e => setText(e.target.value)}
+            <input
+                value={text}
+                onChange={e => setText(e.target.value)}
             />
             <button onClick={() => addTask(text)}>Add</button>
         </div>
